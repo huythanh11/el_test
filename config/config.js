0d16aa57194c0ac9f5810fe34a31d9fcd195d99b
Huy Bryan <huythanhn11@gmail.com>
2025-02-07 22:01:22 +0700

    update config

--- config/config.js
+++ config/config.js
@@ -5,31 +5,8 @@ dotenv.config({
 });
 
 export const environments = {
-    // baseUrl: process.env.BASE_URL || 'https://speechanalyzer.elsaspeak.com/',
-    // apiUrl: process.env.API_URL || 'https://api.example.com',
-    // timeout: parseInt(process.env.TIMEOUT || '120000', 10),
-    // environment: process.env.ENV || 'dev',
-    // browserOptions: {
-    //     headless: process.env.HEADLESS === 'true',
-    //     slowMo: parseInt(process.env.SLOW_MO || '0', 10)
-    // },
-    // testData: {
-    //     users: {
-    //         admin: {
-    //             username: process.env.ADMIN_USER,
-    //             password: process.env.ADMIN_PASSWORD
-    //         }
-    //     }
-    // }
-
     dev: {
-        baseUrl: process.env.DEV_BASE_URL,
-        credentials: {
-            admin: {
-                username: process.env.DEV_ADMIN_USER,
-                password: process.env.DEV_ADMIN_PASSWORD
-            }
-        }
+        baseUrl: process.env.DEV_BASE_URL
     }
 };
 
